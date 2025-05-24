4505dca4004f54c8acefc96d23f2f1b41e0660da
freud87 <feridfreud@gmail.com>
2025-05-24 22:04:57 +0100

    Update kilometrage.js

--- kilometrage.js
+++ kilometrage.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
   // Configuration Supabase
-  const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
+//  const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
+//  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
   // Initialisation du client Supabase
-  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+//  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
   // Fonction pour charger les kilométrages
   async function loadKilometrages() {
     try {
