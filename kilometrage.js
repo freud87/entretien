b94b626517b7dcd722bffea22e6884862f25abf4
freud87 <feridfreud@gmail.com>
2025-05-24 21:55:36 +0100

    Update kilometrage.js

--- kilometrage.js
+++ kilometrage.js
@@ -2,7 +2,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
   // Configuration Supabase
   const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ...'; // raccourci pour lisibilité
+   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
+  // Initialisation du client Supabase
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
   // Chargement des kilométrages
