07e7752197c2410deb6a3d56efbe327746507797
freud87 <feridfreud@gmail.com>
2025-05-24 20:06:20 +0100

    Update historique.js

--- historique.js
+++ historique.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  //const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
-  //const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // remplace par ta vraie clé
-  //const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+  // Configuration Supabase
+  const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
+  // Initialisation du client Supabase
+  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
   async function chargerKilometrages() {
     try {
