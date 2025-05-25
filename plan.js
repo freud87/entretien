e46ed3109e1930133f39bbc149465d3a53e8fd0c
freud87 <feridfreud@gmail.com>
2025-05-25 16:30:40 +0100

    Update plan.js

--- plan.js
+++ plan.js
@@ -1,7 +1,7 @@
 // plan.js — version compatible avec <script src="supabase-js"> déjà chargée
 
 const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // tronqué ici
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function chargerPlan() {
