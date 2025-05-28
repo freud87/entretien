2033d8838c41ba9b45042e8eb0f51efdcf865343
freud87 <feridfreud@gmail.com>
2025-05-28 08:43:26 +0100

    Update historique.js

--- historique.js
+++ historique.js
@@ -1,5 +1,61 @@
-
+document.addEventListener('DOMContentLoaded', async () => {
   // Configuration Supabase
   const supabaseUrl = 'https://ruejiywyrbnlflzyacou.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZWppeXd5cmJubGZsenlhY291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2ODI1NDEsImV4cCI6MjA2MzI1ODU0MX0.MaMVpNzCWdiBufFzhd6RL4riLQQejTUG4FWd5cuHUd8';
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+    async function chargerKilometrages() {
+    try {
+      const { data, error } = await supabase
+        .from('historique')
+        .select('id, date, kilometrage, intervention')
+        .order('date', { ascending: false });
+
+      if (error) throw error;
+
+      const tbody = document.getElementById('table-historique');
+      tbody.innerHTML = '';
+
+      data.forEach((item) => {
+        const row = document.createElement('tr');
+
+        const tdId = document.createElement('td');
+        tdId.textContent = item.id;
+        tdId.classList.add('hidden');
+
+        const tdDate = document.createElement('td');
+        const dateObj = new Date(item.date);
+        tdDate.textContent = dateObj.toLocaleDateString('fr-FR');
+
+        const tdKm = document.createElement('td');
+        tdKm.textContent = item.kilometrage;
+
+        const tdIntervention = document.createElement('td');
+        tdIntervention.textContent = item.intervention;
+
+        const tdProchainkms = document.createElement('td');
+        tdProchainkms.textContent = '';
+        
+        const tdProchaindate = document.createElement('td');
+        tdProchaindate.textContent = '';
+
+       
+
+        row.appendChild(tdId);
+        row.appendChild(tdDate);
+        row.appendChild(tdKm);
+        row.appendChild(tdIntervention);
+        row.appendChild(tdProchainkms);
+        row.appendChild(tdProchaindate);
+
+        tbody.appendChild(row);
+      });
+
+    } catch (error) {
+      console.error('Erreur chargement kilométrages :', error);
+    }
+  }
+
+  await chargerKilometrages();
+});
+
